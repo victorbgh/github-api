17141ba1f595cca439b3e81eacd382febbd353bf
victorhgb <victorhugo-brito@hotmail.com>
2021-03-17 18:16:34 -0300

    configurando o heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/github-api'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + 'dist/github-api/index.html');
+})
+
+app.listen(PORT, () =>{
+    console.log("Servidor iniciado na porta " + PORT);
+});
